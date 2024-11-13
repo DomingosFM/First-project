af2ace72d90f31d107a6a3464ad80cdca6b7edd8
Domingos <46802905+DomingosFM@users.noreply.github.com>
2024-11-13 01:47:02 +0000

    adcionando os arquivos ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home() { 
-   return <h1>That brain of mine is something more than merely mortal; as time will show! 😎</h1>
+function Home() {
+  return (
+    <h1>
+      That brain of mine is something more than merely mortal; as time will
+      show! 😎
+    </h1>
+  );
 }
 
 export default Home;
